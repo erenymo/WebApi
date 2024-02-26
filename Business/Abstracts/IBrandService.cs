e1510e886868bb0ef7f3cfecf951e21fab888666
erenymo <erenbalta342115@gmail.com>
2024-02-26 11:23:53 +0100

    Week 3, Lesson 1

---
+++ Business/Abstracts/IBrandService.cs
@@ -0,0 +1,11 @@
+using Business.Dtos.Requests;
+using Business.Dtos.Responses;
+using Entities.Concretes;
+
+namespace Business.Abstracts;
+
+public interface IBrandService
+{
+    CreatedBrandResponse Add(CreateBrandRequest brand);
+    List<Brand> GetAll();
+}
---
+++ Entities/Concretes/Brand.cs
@@ -0,0 +1,8 @@
+using Core.Entities;
+
+namespace Entities.Concretes;
+
+public class Brand:BaseEntity<int>
+{
+    public string Name { get; set; }
+}
